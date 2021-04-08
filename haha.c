15237b091423b79fe567f4574ed670c9533cf1bd
Göksel <can.onal@ozu.edu.tr>
2021-04-08 22:30:43 +0300

    Add files via upload

---
+++ haha.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#define DEVICE "/dev/exampledevice"
+
+int main(){
+	int i,fd;
+	char ch, write_buf[100], read_buf[100];
+
+	fd = open(DEVICE, O_RDWR);
+
+	if(fd == -1){
+		printf("file %s either does not exist or has been locked by another process\n", DEVICE);
+		exit(-1);
+	}
+	printf("r = read from device,\nw = write to device command: ");
+	scanf("%c",&ch);
+
+	switch(ch){
+		case 'w':
+			printf("enter data: ");
+			scanf(" %[^\n]",(write_buf));
+			write(fd, write_buf, sizeof(write_buf));
+			break;
+		case 'r':
+                	read(fd,read_buf, sizeof(write_buf));
+                	printf("device: %s\n",read_buf);
+                	break; 
+		default:
+			printf("command nor recongnized\n");
+			break;
+	}
+
+	close(fd);	
+
+	return 0;
+}
