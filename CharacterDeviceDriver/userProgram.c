3ef41dc492752f3066db72ca2687ae8dae7fc67f
burakaltintas011801 <72281243+burakaltintas011801@users.noreply.github.com>
2021-06-10 16:07:58 +0300

    added licence

--- CharacterDeviceDriver/userProgram.c
+++ CharacterDeviceDriver/userProgram.c
@@ -1,7 +1,27 @@
+/*
+    This is a Character Device Driver. This Device Driver have read and
+    write functionality.
+    Copyright (C) 2021  Göksel can ÖNAL, 
+    			Nurettin Burak Altıntaş, 
+    			Alperen Fatih Zengin.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see https://www.gnu.org/licenses/.
+    GitHub link:  https://github.com/GokselOnal/CharacterDeviceDriver
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
+
 
 #define DEVICE "/dev/project_char_device" 
 
